4cd7faefd779ceacf71e99a2dc999b35a2dc047f
Holger Caiza <holgerhcdj@hotmail.com>
2022-03-09 08:10:20 -0500

    Switch Case

--- operaciones.js
+++ operaciones.js
@@ -1,5 +1,24 @@
-const n1=parseFloat(prompt("Ingrese la Nota"));
+//const n1=parseFloat(prompt("Ingrese la Nota"));
 //Numero sea válido
 //Este entre 1 y 10 
 //ya sea positivo o negativo
+//[1,10] || [-1,-10]
+       //   Negativos    Positivos
+// -11 -10 -3  -2  -1    1 2 3 
+//    <---------------0------------->
+// let x=-50;
+// if( (x>0 && x<=10) || (x<0 && x>=-11) ){
+//     alert('Numero válido para una nota');
+// }else{
+// 	alert('No vale');
+// }
+//switch
+ const dia=prompt("Ingrese un número");
+ switch(dia){
+ 	case '1': alert('Lunes'); break;
+ 	case '2': alert('Martes'); break;
+ 	default:
+ 	     alert('Valor incorrecto');
+ 	break;
+ }
 
