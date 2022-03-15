9b3828b7df8351927c2333f0cf000b6778ffe386
Holger Caiza <holgerhcdj@hotmail.com>
2022-03-15 08:06:46 -0500

    Funciones Js

--- operaciones.js
+++ operaciones.js
@@ -1,24 +1,17 @@
-//const n1=parseFloat(prompt("Ingrese la Nota"));
-//Numero sea válido
-//Este entre 1 y 10 
-//ya sea positivo o negativo
-//[1,10] || [-1,-10]
-       //   Negativos    Positivos
-// -11 -10 -3  -2  -1    1 2 3 
-//    <---------------0------------->
-// let x=-50;
-// if( (x>0 && x<=10) || (x<0 && x>=-11) ){
-//     alert('Numero válido para una nota');
-// }else{
-// 	alert('No vale');
-// }
-//switch
- const dia=prompt("Ingrese un número");
- switch(dia){
- 	case '1': alert('Lunes'); break;
- 	case '2': alert('Martes'); break;
- 	default:
- 	     alert('Valor incorrecto');
- 	break;
- }
+//Funciones en programación que retornan un resltado
+function divir(n1,n2) {
+	if(n2!=0){
+	    const res=n1/n2;
+	    return res;
+	}else{
+		//es igual a cero
+      return 'No se puede dividir para cero 0';
+	}
+
+}
+const div1=divir(20,5);
+alert(div1);
+
+
+
 
